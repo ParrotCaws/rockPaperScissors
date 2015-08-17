66e5c08ed3d6629af9063d899eabe0702a5c1c95
ParrotCaws <parrott@Ethans-MBP.PK5001Z>
2015-08-17 00:33:17 -0600

    created comparison function

--- rockPaperScissors.go
+++ rockPaperScissors.go
@@ -1,13 +1,48 @@
 package main
 
-import "math/rand"
-
+import (
+	"math/rand"
+	"fmt"
+)
 var choices = []string{"rock", "paper", "scissors"}
-
+var p = fmt.Println
 func randomAnswer() string {
 	return choices[rand.Intn(len(choices))]
 }
 
-func rps(player string, computer string) string {
-	return player //temporary
+
+
+func choiceCompare(choice1 string, choice2 string) {
+	switch{
+	case choice1 == "rock":
+		if choice2 == "rock"{
+			return "It was a tie!!"
+		} else if choice2 == "paper" {
+			return "Player 2 wins!"
+		} else {
+			return "Player 1 wins!"
+		}
+	case choice1 == "paper":
+		if choice2 == "rock" {
+			return "Player 1 wins!"
+		} else if choice2 == "paper" {
+			return "It was a tie!"
+		} else {
+			return "Player 2 wins!"
+		}
+	case choice1 == "scissors":
+		if choice2 == "rock" {
+			return "Player 2 wins!"
+		} else if choice2 == "paper" {
+			return "Player 1 wins!"
+		} else {
+			return "It was a tie!"
+		}
+	default:
+		return "There was an error!"
+}
+
+func main() {c
+	var result = choiceCompare("rock", "scissors")
+	p(result)
 }
